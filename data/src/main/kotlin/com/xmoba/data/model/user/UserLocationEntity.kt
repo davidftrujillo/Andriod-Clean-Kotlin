4c67b217b4cdfc5851adce2c7fea77c56b0cd535
David <david.fernandez.trujillo@gmail.com>
2018-08-07 20:19:03 +0200

    Add params to request

--- data/src/main/kotlin/com/xmoba/data/model/user/UserLocationEntity.kt
+++ data/src/main/kotlin/com/xmoba/data/model/user/UserLocationEntity.kt
@@ -1,5 +1,7 @@
 package com.xmoba.data.model.user
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by david on 6/8/18.
  */
@@ -7,7 +9,7 @@ data class UserLocationEntity(
         val street: String,
         val city: String,
         val state: String,
-        val postCode: Int,
+        @SerializedName("postcode") val postCode: Int,
         val coordinates: CoordinatesEntity,
         val timezone: TimezoneEntity?
 )
