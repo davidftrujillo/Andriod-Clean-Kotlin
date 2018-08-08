ac96e62688eda3911f4c6686a0f1a0e5e17c8069
David <david.fernandez.trujillo@gmail.com>
2018-08-08 21:16:01 +0200

    Add converters for embedded objects

---
+++ data/src/main/kotlin/com/xmoba/data/model/database/converters/TimezoneConverter.kt
@@ -0,0 +1,28 @@
+package com.xmoba.data.model.database.converters
+
+import android.arch.persistence.room.TypeConverter
+import com.google.gson.Gson
+import com.xmoba.data.model.database.converters.ListStringConverter.fromJson
+import com.xmoba.data.model.user.TimezoneEntity
+
+/**
+ * Created by david on 8/8/18.
+ */
+object TimezoneConverter {
+
+    private val gson = Gson()
+
+    @TypeConverter
+    @JvmStatic
+    fun timezoneToString(list: TimezoneEntity): String {
+
+        return gson.toJson(list)
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun stringToTimezone(s: String): TimezoneEntity {
+
+        return gson.fromJson<TimezoneEntity>(s)
+    }
+}
